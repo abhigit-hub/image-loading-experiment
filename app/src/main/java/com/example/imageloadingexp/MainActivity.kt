d61cc4ba852a77f7c498a40e4f883b1ffffaf7c9
Abhijit Naik <naikabhiit@gmail.com>
2021-11-29 21:02:15 +0530

    Added a proposed solution(Util class) from Sony Developer site by Andreas

--- app/src/main/java/com/example/imageloadingexp/MainActivity.kt
+++ app/src/main/java/com/example/imageloadingexp/MainActivity.kt
@@ -1,19 +1,115 @@
 package com.example.imageloadingexp
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
 import com.bumptech.glide.Glide
+import com.example.imageloadingexp.ScalingUtilities.ScalingLogic
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_jpeg.*
 
+
 class MainActivity : AppCompatActivity() {
+
+    /** Id of image resource to decode  */
+    private var mSourceId = 0
+
+    /** Wanted width of decoded image  */
+    private val mDstWidth = 200
+
+    /** Wanted height of decoded image  */
+    private val mDstHeight = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initJPEGRow()
+        initNewRow()
+    }
+
+    private fun initNewRow() {
+        mSourceId = R.drawable.dummy1
+
+        button_scaling_bad.setOnClickListener {
+            badButtonPressed()
+        }
+        button_scaling_crop.setOnClickListener {
+            cropButtonPressed()
+        }
+        button_scaling_fit.setOnClickListener {
+            fitButtonPressed()
+        }
+    }
+
+    private fun badButtonPressed() {
+        val startTime: Long = SystemClock.uptimeMillis()
+
+        // Part 1: Decode image
+        val unscaledBitmap = BitmapFactory.decodeResource(resources, mSourceId)
+
+        // Part 2: Scale image
+        val scaledBitmap = Bitmap
+                .createScaledBitmap(unscaledBitmap, mDstWidth, mDstHeight, true)
+        unscaledBitmap.recycle()
+
+        // Calculate memory usage and performance statistics
+        val memUsageKb = unscaledBitmap.rowBytes * unscaledBitmap.height / 1024
+        val stopTime: Long = SystemClock.uptimeMillis()
+
+        // Publish results
+        text_result.text = ("Time taken: " + (stopTime - startTime)
+                + " ms. Memory used for scaling: " + memUsageKb + " kb.")
+        image.setImageBitmap(scaledBitmap)
+    }
+
+    private fun cropButtonPressed() {
+        val startTime = SystemClock.uptimeMillis()
+
+        // Part 1: Decode image
+        val unscaledBitmap = ScalingUtilities.decodeResource(resources, mSourceId,
+                mDstWidth, mDstHeight, ScalingLogic.CROP)
+
+        // Part 2: Scale image
+        val scaledBitmap = ScalingUtilities.createScaledBitmap(unscaledBitmap, mDstWidth,
+                mDstHeight, ScalingLogic.CROP)
+        unscaledBitmap.recycle()
+
+        // Calculate memory usage and performance statistics
+        val memUsageKb = unscaledBitmap.rowBytes * unscaledBitmap.height / 1024
+        val stopTime = SystemClock.uptimeMillis()
+
+        // Publish results
+        text_result.text = ("Time taken: " + (stopTime - startTime)
+                + " ms. Memory used for scaling: " + memUsageKb + " kb.")
+        image.setImageBitmap(scaledBitmap)
+    }
+
+    private fun fitButtonPressed() {
+        val startTime = SystemClock.uptimeMillis()
+
+        // Part 1: Decode image
+        val unscaledBitmap = ScalingUtilities.decodeResource(resources, mSourceId,
+                mDstWidth, mDstHeight, ScalingLogic.FIT)
+
+        // Part 2: Scale image
+        val scaledBitmap = ScalingUtilities.createScaledBitmap(unscaledBitmap, mDstWidth,
+                mDstHeight, ScalingLogic.FIT)
+        unscaledBitmap.recycle()
+
+        // Calculate memory usage and performance statistics
+        val memUsageKb = unscaledBitmap.rowBytes * unscaledBitmap.height / 1024
+        val stopTime = SystemClock.uptimeMillis()
+
+        // Publish results
+        text_result.text = ("Time taken: " + (stopTime - startTime)
+                + " ms. Memory used for scaling: " + memUsageKb + " kb.")
+        image.setImageBitmap(scaledBitmap)
     }
 
     private fun initJPEGRow() {
