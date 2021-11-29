47e39059b0ffdc4f2e66a3fcc9e1a30a52c306c1
Abhijit Naik <naikabhiit@gmail.com>
2021-11-29 14:05:52 +0530

    5 variants of Image Loading libraries used

--- app/src/main/java/com/harman/imageloadingexp/MainActivity.kt
+++ app/src/main/java/com/harman/imageloadingexp/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.harman.imageloadingexp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.layout_jpeg.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initJPEGRow()
+    }
+
+    private fun initJPEGRow() {
+        Glide.with(this)
+            .load("https://demo-web-harman.demandware.net/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw37446e91/Astra_Hero_Black-1605x1605px.png")
+            .override(500, 500)
+            .into(c2)
+
+        Picasso.with(this)
+            .load("https://demo-web-harman.demandware.net/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw37446e91/Astra_Hero_Black-1605x1605px.png")
+            .resize(1000, 1000)
+            .into(c3)
+
+        c4.load("https://demo-web-harman.demandware.net/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw37446e91/Astra_Hero_Black-1605x1605px.png")
+
+        val uri = Uri.parse("android.resource://com.harman.audiofocus1/drawable/dummy1")
+        c5.setImageURI("https://demo-web-harman.demandware.net/on/demandware.static/-/Sites-masterCatalog_Harman/default/dw37446e91/Astra_Hero_Black-1605x1605px.png")
     }
 }
